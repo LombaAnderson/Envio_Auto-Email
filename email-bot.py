408af7386952c2d2509297fcfb2244991a4d7988
Anderson Lomba <andersonlomba172@gmail.com>
2021-01-05 11:40:30 -0300

    Como enviar emails com reconhecimento de voz usando Python

---
+++ email-bot.py
@@ -0,0 +1,61 @@
+import smtplib
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def get_info():
+        try:
+            with sr.Microphone() as source:
+                print('listening...')
+                voice= listener.listen(source)
+                info=listener.recognize_google(voice)
+                print(info)
+                return info.lower()
+
+        except:
+            pass
+
+def send_email(receiver, subject, message):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls=()
+        server.login('fresh.grandma21@gmail.com', 'GrandmaDatingSantaOnChristmas')
+        email = EmailMessage()
+        email['From'] = 'fresh.grandma21@gmail.com'
+        email ['To'] = receiver
+        email['Subject'] = subject
+        email.set_content(message)
+        server.send_message(email)
+        
+
+email_list = {
+'dude': 'exempleofemail@gmail.com',
+'Janice': 'anotherexemplo@gmail.com'
+}
+        
+def get_email_info():
+    talk('To Whom you want to send email')
+    name = get_info()
+    receiver = email_list[name]
+    print(receiver)
+    talk('What is the subject of your email?')
+    subject = get_info()
+    talk('Tell me the text in your email')
+    message = get_info()
+
+    send_email(receiver,subject,message)
+    talk('Hey your email is sent')
+    talk('Do you want to send more email?')
+    send_more = get_info()
+    if 'yes' in send_more:
+        get_email_info()
+
+
+get_email_info()
+
